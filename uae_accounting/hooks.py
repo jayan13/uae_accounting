ee2d7aefc2312cc21fba670186979b51e2505fc8
jayan13 <jayakumar13s@gmail.com>
2024-04-15 21:33:37 +0530

    reports

--- uae_accounting/hooks.py
+++ uae_accounting/hooks.py
@@ -227,3 +227,12 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    { "doctype": "Report", "filters": [ ["name", "in", 
+		( 
+			"UAE VAT201",
+			"Trial Balance for Auditor",
+		)
+		
+	] ] }
+]
